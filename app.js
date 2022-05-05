c65ae8a78a34a7764c1d378abe66cb24280e3489
Mbalenhle Ndaba <mbalindaba01@gmail.com>
2022-05-05 22:52:06 +0200

    set up data required to calculate totals

--- app.js
+++ app.js
@@ -3,3 +3,33 @@ import Alpine from 'alpinejs'
 window.Alpine = Alpine
  
 Alpine.start()
+
+document.addEventListener('alpine:init', () => {
+    Alpine.data('pizzas', () => ({
+        smallCount: 0,
+        medCount: 0,
+        largeCount: 0,
+        smallPrice: 50.99,
+        medPrice: 80.99,
+        largePrice: 109.99,
+        addPizza(size){
+            size++
+        },
+        removePizza(size){
+            size--
+        },
+        getSmallTotal(){
+            return smallCount * smallPrice
+        },
+        getMedTotal(){
+            return medCount * medPrice
+        },
+        getLargeTotal(){
+            return largeCount * largePrice
+        },
+
+        getTotal(){
+            return getSmallTotal + getMedTotal + getLargeTotal
+        }
+    }))
+})

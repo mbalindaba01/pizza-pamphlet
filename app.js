21a7f78a3a8fe63531a5f40d1c6b454c0cae2b2e
Mbalenhle Ndaba <mbalindaba01@gmail.com>
2022-05-06 02:15:53 +0200

    added total calculation logic and feedback texts

--- app.js
+++ app.js
@@ -1,35 +1,63 @@
-import Alpine from 'alpinejs'
- 
-window.Alpine = Alpine
- 
-Alpine.start()
-
+console.log('it is working')
 document.addEventListener('alpine:init', () => {
     Alpine.data('pizzas', () => ({
         smallCount: 0,
         medCount: 0,
         largeCount: 0,
-        smallPrice: 50.99,
-        medPrice: 80.99,
-        largePrice: 109.99,
-        addPizza(size){
-            size++
+        smallPrice: 48.99,
+        medPrice: 78.99,
+        largePrice: 114.99,
+        tendered: '',
+        addSmall(){
+            this.smallCount++
         },
-        removePizza(size){
-            size--
+        addMed(){
+            this.medCount++
         },
+        addLarge(){
+            this.largeCount++
+        },
+        removeSmall(){
+            this.smallCount--
+        },
+
+        removeMed(){
+            this.medCount--
+        },
+
+        removeLarge(){
+            this.largeCount--
+        },
+
+
         getSmallTotal(){
-            return smallCount * smallPrice
+            return this.smallCount * this.smallPrice
         },
         getMedTotal(){
-            return medCount * medPrice
+            return this.medCount * this.medPrice
         },
         getLargeTotal(){
-            return largeCount * largePrice
+            return this.largeCount * this.largePrice
         },
 
         getTotal(){
-            return getSmallTotal + getMedTotal + getLargeTotal
+            let total = this.getSmallTotal() + this.getMedTotal() + this.getLargeTotal()
+            return total.toFixed(2)
+        },
+
+        feedbackText(){
+            if(this.getTotal() > parseFloat(this.tendered)){
+                return 'Insufficient funds'
+            }else if(this.getTotal() <= parseFloat(this.tendered)){
+                return 'Funds are enough. Let\'s checkout'
+            }
+        },
+
+        checkout(){
+            this.smallCount = 0
+            this.medCount = 0
+            this.largeCount = 0
+            window.alert('Successful Purchase')
         }
     }))
 })
